952a557f80e0445196331abae67f61a93dd70e1a
Ivan Guanipa <ivanguanipa@gmail.com>
2022-02-22 17:42:47 -0400

    fix gears icon and file system quota

--- resources/views/vendor/laravel-filemanager/index.blade.php
+++ resources/views/vendor/laravel-filemanager/index.blade.php
@@ -41,9 +41,6 @@
         <span class="d-none d-lg-inline">{{ trans('laravel-filemanager::lfm.menu-multiple') }}</span>
       </a>
     </div>
-    <a class="navbar-toggler collapsed border-0 px-1 py-2 m-0" data-toggle="collapse" data-target="#nav-buttons">
-      <i class="fas fa-cog fa-fw"></i>
-    </a>
     <div class="collapse navbar-collapse flex-grow-0" id="nav-buttons">
       <ul class="navbar-nav">
         <li class="nav-item">
