116f8b006bc527f56875219b16d7f0c321263b59
Ardilan <dilancorr@gmail.com>
2022-05-09 23:09:14 -0500

    Ajuste menu quiz

--- resources/views/web/default/panel/quizzes/start.blade.php
+++ resources/views/web/default/panel/quizzes/start.blade.php
@@ -2,61 +2,57 @@
 
 @section('content')
     <div class="container">
-        <section class="mt-40">
-            <h2 class="font-weight-bold font-16 text-dark-blue">{{ $quiz->title }}</h2>
-            <div class="nav-icons-or-start-live navbar-order">
-                <a href="{{ $quiz->webinar->getUrl() }}" class="btn btn-sm btn-primary nav-start-a-live-btn">{{ trans('quiz.course_back') }}</a>
+        <section class="mt-40" style="display: flex; justify-content: space-between; align-items: center; margin-top: 5%;">
+            <div>
+                <h2 class="font-weight-bold font-16 text-dark-blue">{{ $quiz->title }}</h2>
+                <p class="text-gray font-14 mt-5">
+                    <a href="{{ $quiz->webinar->getUrl() }}" target="_blank" class="text-gray">{{ $quiz->webinar->title }}</a>
+                    | {{ trans('public.by') }}
+                    <span class="font-weight-bold">
+                        <a href="{{ $quiz->creator->getProfileUrl() }}" target="_blank" class="font-14"> {{ $quiz->creator->full_name }}</a>
+                    </span>
+                </p>
             </div>
-            <p class="text-gray font-14 mt-5">
-                <a href="{{ $quiz->webinar->getUrl() }}" target="_blank" class="text-gray">{{ $quiz->webinar->title }}</a>
-                | {{ trans('public.by') }}
-                <span class="font-weight-bold">
-                    <a href="{{ $quiz->creator->getProfileUrl() }}" target="_blank" class="font-14"> {{ $quiz->creator->full_name }}</a>
-                </span>
-            </p>
-
-            <div class="activities-container shadow-sm rounded-lg mt-25 p-20 p-lg-35">
-                <div class="row">
+            <div class="activities-container shadow-sm rounded-lg p-20" style="width: 60%;">
+                <div class="row" style="gap: 2%; flex-wrap: nowrap; margin-right: 0; margin-left: -5%;">
                     <div class="col-6 col-md-3 mt-30 mt-md-0 d-flex align-items-center justify-content-center">
                         <div class="d-flex flex-column align-items-center text-center">
-                            <img src="/assets/default/img/activity/58.svg" width="64" height="64" alt="">
-                            <strong class="font-30 font-weight-bold text-secondary mt-5">{{  $quiz->pass_mark }}/{{  $quizQuestions->sum('grade') }}</strong>
-                            <span class="font-16 text-gray">{{ trans('public.min') }} {{ trans('quiz.grade') }}</span>
+                            <strong class="font-20 font-weight-bold text-secondary mt-5">{{  $quiz->pass_mark }}/{{  $quizQuestions->sum('grade') }}</strong>
+                            <span class="font-12 text-gray">{{ trans('public.min') }} {{ trans('quiz.grade') }}</span>
                         </div>
                     </div>
 
                     <div class="col-6 col-md-3 mt-30 mt-md-0 d-flex align-items-center justify-content-center">
                         <div class="d-flex flex-column align-items-center text-center">
-                            <img src="/assets/default/img/activity/88.svg" width="64" height="64" alt="">
-                            <strong class="font-30 font-weight-bold text-secondary mt-5">{{ $attempt_count }}/{{ $quiz->attempt }}</strong>
-                            <span class="font-16 text-gray">{{ trans('quiz.attempts') }}</span>
+                            <strong class="font-20 font-weight-bold text-secondary mt-5">{{ $attempt_count }}/{{ $quiz->attempt }}</strong>
+                            <span class="font-12 text-gray">{{ trans('quiz.attempts') }}</span>
                         </div>
                     </div>
 
                     <div class="col-6 col-md-3 mt-30 mt-md-0 d-flex align-items-center justify-content-center mt-5 mt-md-0">
                         <div class="d-flex flex-column align-items-center text-center">
-                            <img src="/assets/default/img/activity/47.svg" width="64" height="64" alt="">
-                            <strong class="font-30 font-weight-bold text-secondary mt-5">{{ $quizQuestions->count() }}</strong>
-                            <span class="font-16 text-gray">{{ trans('public.questions') }}</span>
+                            <strong class="font-20 font-weight-bold text-secondary mt-5">{{ $quizQuestions->count() }}</strong>
+                            <span class="font-12 text-gray">{{ trans('public.questions') }}</span>
                         </div>
                     </div>
 
                     <div class="col-6 col-md-3 mt-30 mt-md-0 d-flex align-items-center justify-content-center mt-5 mt-md-0">
                         <div class="d-flex flex-column align-items-center text-center">
-                            <img src="/assets/default/img/activity/clock.svg" width="64" height="64" alt="">
-                            <strong class="font-30 font-weight-bold text-secondary mt-5">
-                                @if ($quiz->time && (int)$quiz->time > 0)
-                                    
-                                <div class="d-flex align-items-center timer" data-minutes-left="{{ $quiz->time }}"></div>
+                            <strong class="font-20 font-weight-bold text-secondary mt-5">
+                                @if ($quiz->time && (int)$quiz->time > 0)                             
+                                    <div class="d-flex align-items-center timer" data-minutes-left="{{ $quiz->time }}"></div>
                                 @else
                                     {{trans('public.unlimited')}}
                                 @endif
                             </strong>
-                            <span class="font-16 text-gray">{{ trans('quiz.remaining_time') }}</span>
+                            <span class="font-12 text-gray">{{ trans('quiz.remaining_time') }}</span>
                         </div>
                     </div>
 
                 </div>
+            </div>
+            <div class="nav-icons-or-start-live navbar-order">
+                <a href="{{ $quiz->webinar->getUrl() }}" class="btn btn-sm btn-primary nav-start-a-live-btn">{{ trans('quiz.course_back') }}</a>
             </div>
         </section>
 
